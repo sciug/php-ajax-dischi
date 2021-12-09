81310ac0bfed6ac166e5450ff46180c1154f5e20
sciug <gaetano.sciurti@libero.it>
2021-12-09 14:18:39 +0100

    add filter

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,12 @@ const app = new Vue({
     el: '#app',
     data:{
         albums: null,
-        loading:true
+        loading:true,
+        error: "",
+        selectedGenre: "",
+        selectedAuthor: "",
+        selectedA:"",
+        selectedG:"",
     },
     mounted(){
         axios
@@ -14,5 +19,36 @@ const app = new Vue({
         }).catch(error =>{
             console.log(error)
         })
-    }
+    },
+    methods:{
+        filterGenre() {
+            this.selectedGenre = this.selectedG;
+          },
+          filterAuthor() {
+            this.selectedAuthor = this.selectedA;
+          
+          },
+    },
+    computed: {
+        getFilterGenres() {
+          console.log(this.selectedGenre);
+          if (this.selectedGenre == "" || this.selectedGenre == "all") {
+            return this.albums;
+          }
+          const filtered = this.albums.filter((album) => {
+            return album.genre == this.selectedGenre;
+          });
+          return filtered;
+        },
+        getFilterAuthors() {
+          if (this.selectedAuthor == "") {
+            return this.getFilterGenres;
+          }
+          console.log(this.getFilterGenres);
+          const filteredAuthors = this.getFilterGenres.filter((getFilterGenre) => {
+            return getFilterGenre.author == this.selectedAuthor;
+          });
+          return filteredAuthors;
+        },
+      },
 })
