27cf85b9defb3e8e4b0fdb79cde4e27bcb697a3a
sciug <gaetano.sciurti@libero.it>
2021-12-09 14:23:43 +0100

    change filter

--- index.php
+++ index.php
@@ -38,23 +38,13 @@
     <div class="col-4">
       <label for="author" class="select-label">select author</label>,
        <select v-model="selectedA" name="author" id="author" class="select" @change="filterAuthor" placeholder="select author">
-                    <option value="Bon Jovi"><span>Bon Jovi</span></option>
-                    <option value="Queen"><span>Queen</span></option>
-                    <option value="Sting"><span>Sting</span></option>
-                    <option value="Steve Gadd Band"><span>Steve Gadd Band</span></option>
-                    <option value="Iron Maiden"><span>Iron Maiden</span></option>
-                    <option value="Deep Purple"><span>Deep Purple</span></option>
-                    <option value="Metallica"><span>Metallica</span></option>
-                    <option value="Dave Weckl"><span>Dave Weckl</span></option>
-                    <option value="Michael Jacjson"><span>Michael Jacjson</span></option>
+      
+                    <option :value="album.author"  v-for="album in getFilterAuthors"><span>{{album.author}}</span></option>
+                    
         </select> 
         <label for="genre" class="select-label">select genre</label>
        <select v-model="selectedG" name="genre" id="genre" class="select" @change="filterGenre" placeholder="select author">
-                    <option value="Rock"><span>Rock</span></option>
-                    <option value="Jazz"><span>Jazz</span></option>
-                    <option value="Metal"><span>Metal</span></option>
-                    <option value="Pop"><span>Pop</span></option>
-                    <option value="all"><span>all genre</span></option>
+       <option :value="album.genre"  v-for="album in getFilterAuthors"><span>{{album.genre}}</span></option>
                    
         </select>
 
